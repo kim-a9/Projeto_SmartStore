11b0c2897cb2578988f345285f56e109958978f3
kim-a9 <kimberlysantosalves@gmail.com>
2025-10-08 21:15:18 -0300

    test: integration tests

--- src/server.ts
+++ src/server.ts
@@ -25,3 +25,4 @@ async function startServer() {
 startServer();
 
 
+export default app;
--- tests/integration/Product.spec.ts
+++ tests/integration/Product.spec.ts
@@ -1,18 +1,55 @@
-import supertest from 'supertest';
+import request from 'supertest';
 import mongoose from 'mongoose';
 import { ProductModel } from '../../src/database/MongooseProductModel';
-
-describe('Product unit test ' , () => {
-    let prodId: mongoose.Types.ObjectId;
+import app from '../../src/server';
+describe('Product integration test ' , () => {
 
     beforeAll(async () => {
-        await mongoose.connect(process.env.MONGODB_TEST!);
+        await mongoose.createConnection(process.env.MONGODB_TEST!);
     });
 
     beforeEach(async () => {
-        ProductModel.deleteMany({});
+        await ProductModel.deleteMany({});
     });
 
+    afterAll(async () => {
+        await ProductModel.deleteMany({});
+        await mongoose.connection.close();
+    });
+
+    it('deve retornar 200 e mensagem inicial', async () =>{
+        const res = await request(app).get('/');
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body.message).toEqual('Olá, Mundo');
+
+    });
+
+    it('deve criar novo produto e retornar 200', async () => {
+        const res = await request(app).post('/cadastro').send({
+            productCode: "0001" ,
+            name: "produto 1",
+            quantity: 50,
+            category: "categoria",
+            price: 5.00
+        });
+
+        expect(res.body.message).toEqual('Produto criado com sucesso!');
+        expect(res.statusCode).toBe(200);
+    });
+
+    it('deve retornar erro ao adicionar produto com campos inválidos', async () =>{
+           const res = await request(app).post('/cadastro').send({
+            productCode: "" ,
+            name: "",
+            quantity: 50,
+            category: "categoria",
+            price: 5.00
+        });
+        
+
+        expect(res.statusCode).toBe(500);
+    })
 
 
 
