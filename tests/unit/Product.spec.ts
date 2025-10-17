1bed7400926155bb5174c22eb51b939f6cbad900
kim-a9 <kimberlysantosalves@gmail.com>
2025-10-17 20:38:56 -0300

    test: testes getAllProducts

--- tests/unit/Product.spec.ts
+++ tests/unit/Product.spec.ts
@@ -3,11 +3,14 @@ import { ProductRepository } from "../../src/repository/product-repository";
 
 describe("Product unit test ", () => {
   let createProd: ProductServices;
+  let getAllProd: ProductServices;
   let prodRepo: ProductRepository;
 
   beforeEach(() => {
     prodRepo = new ProductRepository();
     createProd = new ProductServices(prodRepo);
+    getAllProd = new ProductServices(prodRepo);
+
   });
 
   beforeAll(() => {
@@ -50,6 +53,37 @@ describe("Product unit test ", () => {
     const prodService = new ProductServices(mockRepository);
 
     expect(mockRepository.create).not.toHaveBeenCalled();
+  });
+
+  it("deve consultar todos os produtos registrados com sucesso", async () => {
+      const newProd = [{
+        productCode: "1235",
+        name: "Produto 1",
+        quantity: 50,
+        category: "categoria",
+        price: 5.0,
+    }, 
+      {
+        productCode: "5678",
+        name: "Produto 2",
+        quantity: 50,
+        category: "categoria",
+        price: 5.0
+      }];
+
+    const mockRepository = {
+      create: jest.fn().mockResolvedValue(newProd),
+      getAll: jest.fn().mockResolvedValue(newProd),
+    };
+    const prodService = new ProductServices(mockRepository);
+    const product = await prodService.createProduct(newProd as any);
+
+    const getProd = await prodService.getProducts();
+
+    expect(getProd).not.toBeNull();
+    expect(mockRepository.getAll).toHaveBeenCalledTimes(1);
+    expect(mockRepository.getAll).toHaveReturnedTimes(1);
+
   });
 
 
