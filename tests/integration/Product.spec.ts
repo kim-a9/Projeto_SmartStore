27bb657b6a132518f77487feb460054f00447598
kim-a9 <kimberlysantosalves@gmail.com>
2025-10-24 21:41:28 -0300

    fix: teste integração

--- tests/integration/Product.spec.ts
+++ tests/integration/Product.spec.ts
@@ -114,9 +114,11 @@ describe('Product integration test ' , () => {
             price: 5.00
         });
 
-        const r = await request(app).delete(`/excluir/${9876}`);
+        // const r = await request(app).delete(`/excluir/${9876}`);
 
-        expect(r.status).toBe(201);
+        expect(await request(app).delete(`/excluir/${9876}`)).toBe(201);
+
+        // expect(r.statusCode).toBe(201);
     });
 
 
