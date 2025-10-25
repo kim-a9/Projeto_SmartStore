d12ff0485e471a9e81164c79f33e91b9e8b9b677
kim-a9 <kimberlysantosalves@gmail.com>
2025-10-25 19:07:50 -0300

    fix: update integration test

--- tests/integration/Product.spec.ts
+++ tests/integration/Product.spec.ts
@@ -91,15 +91,16 @@ describe('Product integration test ' , () => {
     });
 
     it('deve atualizar informações de um produto', async () =>{
-        const res = await request(app).post('/cadastro').send({
-            productCode: "9876" ,
-            name: "produto",
+        const prod = await ProductModel.create({
+            productCode: "9876",
+            name: "Produto 1",
             quantity: 50,
             category: "categoria",
-            price: 5.00
+            price: 5.0,
         });
+        const res = await request(app).post('/cadastro').send(prod);
 
-        const r = await request(app).put(`/editar/${9876}`).send({
+        const r = await request(app).put(`/editar/${prod.productCode}`).send({
             name: "Produto Editado"
         });
         
