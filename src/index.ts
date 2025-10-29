0e57ff4cf5b8df89720d39a9d4f06002940ea3f4
kim-a9 <kimberlysantosalves@gmail.com>
2025-10-29 20:32:08 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes/product-routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
-
-
+app.use(cors());
 
 
 export default app;
